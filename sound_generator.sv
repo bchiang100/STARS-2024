d82455665389cf69b1a928f9c265d50a35c01c7c
bchiang100 <bryanchiang100@gmail.com>
2024-06-21 20:16:54 +0000

    pushing to GitHub

--- sound_generator.sv
+++ sound_generator.sv
@@ -22,7 +22,7 @@ MODE_TYPES mode_o;
 
 pos_detector posDet (.goodColl(goodColl), .badColl(badColl), .button(button), .direction(direction), .clk(clk), .nRst(nRst), .goodColl_i(.goodColl_i), .badColl_i(badColl_i), .button_i(button_i), .direction_i(direction_i));
 freq_selector freqSel (.freq(freq), .goodColl_i(goodColl_i), .badColl_i(badColl_i), .direction_i(direction_i));
-sound_fsm fsm1 (.playSound(playSound), .mode_o(mode_o), .clk(clk), .nRst(nRst), .goodColl(goodColl), .badColl(badColl), .button(button), .direction(direction));
-oscillator osc1 (.at_max(at_max), .clk(clk), .nRst(nRst), .freq(freq), .state(mode_o), .playSound(playSound));
-dac_counter dac1 (.dacCount(dacCount), .clk(clk), .nRst(nRst), .at_max(at_max));
+sound_fsm fsm (.playSound(playSound), .mode_o(mode_o), .clk(clk), .nRst(nRst), .goodColl(goodColl), .badColl(badColl), .button(button), .direction(direction));
+oscillator osc (.at_max(at_max), .clk(clk), .nRst(nRst), .freq(freq), .state(mode_o), .playSound(playSound));
+dac_counter dac (.dacCount(dacCount), .clk(clk), .nRst(nRst), .at_max(at_max));
 endmodule
