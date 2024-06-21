90aa4c39c00e05e98d25458b4a3f63c1e5e728a1
bchiang100 <bryanchiang100@gmail.com>
2024-06-21 20:15:25 +0000

    pushing to GitHub

--- sound_generator.sv
+++ sound_generator.sv
@@ -8,16 +8,21 @@ module sound_generator
     parameter N = 7
 )
 (
-    input logic clk, nRst, goodColl, badColl, buttonPressed,
-    input logic [3:0] direction,
+    input logic clk, nRst, goodColl_i, badColl_i, buttonPressed_i,
+    input logic [3:0] direction_i,
     output logic [N - 1:0] dacCount
 );
+
 logic [8:0] freq;
+logic goodColl, badColl, button;
+logic [3:0] direction;
 logic playSound;
 logic at_max;
 MODE_TYPES mode_o;
 
-sound_fsm fsm1 (.playSound(playSound), .mode_o(mode_o), .freq(freq), .clk(clk), .nRst(nRst), .goodColl(goodColl), .badColl(badColl), .buttonPressed(buttonPresed), .direction(direction));
+pos_detector posDet (.goodColl(goodColl), .badColl(badColl), .button(button), .direction(direction), .clk(clk), .nRst(nRst), .goodColl_i(.goodColl_i), .badColl_i(badColl_i), .button_i(button_i), .direction_i(direction_i));
+freq_selector freqSel (.freq(freq), .goodColl_i(goodColl_i), .badColl_i(badColl_i), .direction_i(direction_i));
+sound_fsm fsm1 (.playSound(playSound), .mode_o(mode_o), .clk(clk), .nRst(nRst), .goodColl(goodColl), .badColl(badColl), .button(button), .direction(direction));
 oscillator osc1 (.at_max(at_max), .clk(clk), .nRst(nRst), .freq(freq), .state(mode_o), .playSound(playSound));
 dac_counter dac1 (.dacCount(dacCount), .clk(clk), .nRst(nRst), .at_max(at_max));
 endmodule
