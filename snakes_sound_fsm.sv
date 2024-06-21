1af8a0cb9ffcb5899315db9548423b46fffc5c40
bchiang100 <bryanchiang100@gmail.com>
2024-06-21 20:04:32 +0000

    pushing to GitHub

--- snakes_sound_fsm.sv
+++ snakes_sound_fsm.sv
@@ -1,29 +1,16 @@
-// THIS MODULE CONTAINS THE POSITIVE EDGE DETECTOR, FSM LOGIC, AND FREQUENCY SELECTOR LOGIC
-
 typedef enum logic {
     OFF = 1'b0,
     ON = 1'b1
 } MODE_TYPES;
 
 module sound_fsm(
-    input logic clk, nRst, goodColl, badColl, buttonPressed,
+    input logic clk, nRst, goodColl, badColl, button,
     input logic [3:0] direction,
     output logic playSound,
-    output logic [8:0] freq,
     output MODE_TYPES mode_o // current state
 );
-logic newGoodColl, newBadColl;
-logic [3:0] newDirection;
-
-
 MODE_TYPES next_state;
 
-// Positive Edge Detector
-always_ff @(posedge clk) begin
-    newGoodColl <= goodColl;
-    newBadColl <= badColl;
-    newDirection <= direction;
-end
 always_ff @(posedge clk, negedge nRst) begin
     if (~nRst) begin
         mode_o <= ON;
@@ -37,28 +24,17 @@ always_comb begin
     next_state = mode_o;
     case (mode_o)
         ON:
-            if (buttonPressed) begin
+            if (button) begin
                 next_state = OFF;
             end
-            else if (newGoodColl || newBadColl || |newDirection) begin
+            else if (goodColl || badColl || |direction) begin
                 playSound = 1'b1;
             end
         OFF:
-            if (buttonPressed) begin
+            if (button) begin
                 next_state = ON;
             end
     endcase
 end
 
-// Frequency Selector
-always_comb begin
-    freq = 0;
-    if (goodColl)
-        freq = 440; // A
-    if (badColl)
-        freq = 311; // D Sharp
-    if (|direction)
-        freq = 262; // C
-end
 endmodule
-
